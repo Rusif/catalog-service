18eb2697b0f689665a175996ed69ee66fe429cfc
Rusif Budagov <rusifbudagov@Rusifs-MacBook-Pro.local>
2024-01-07 22:05:32 +0400

    Image Publisher was added

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -31,7 +31,7 @@ public record Book(
                 message = "The book price must be greater than zero."
         )
         Double price,
-
+        String publisher,
         @CreatedDate
         Instant createdDate,
 
@@ -42,10 +42,10 @@ public record Book(
 
 ) {
     public static Book of(
-            String isbn, String title, String author, Double price
+            String isbn, String title, String author, Double price, String publisher
     ) {
         return new Book(
-                null, isbn, title, author, price, null, null, 0
+                null, isbn, title, author, price, publisher, null, null, 0
         );
     }
 }

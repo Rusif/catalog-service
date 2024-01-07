796f6b4fe3c6660669241ceff190d981ae149741
Rusif Budagov <rusifbudagov@Rusifs-MacBook-Pro.local>
2024-01-07 18:49:14 +0400

    Impletended

--- src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
@@ -1,13 +1,14 @@
 package com.polarbookshop.catalogservice.domain;
 
-import java.util.Set;
-
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BookValidationTests {
@@ -21,16 +22,14 @@ class BookValidationTests {
 
     @Test
     void whenAllFieldsCorrectThenValidationSucceeds() {
-        var book =
-        new Book("1234567890", "Title", "Author", 9.90);
+        var book = Book.of("1234567890", "Title", "Author", 9.90);
         Set<ConstraintViolation<Book>> violations = validator.validate(book);
         assertThat(violations).isEmpty();
     }
 
     @Test
     void whenIsbnDefinedButIncorrectThenValidationFails() {
-        var book =
-        new Book("a234567890", "Title", "Author", 9.90);
+        var book = Book.of("a234567890", "Title", "Author", 9.90);
         Set<ConstraintViolation<Book>> violations = validator.validate(book);
         assertThat(violations).hasSize(1);
         assertThat(violations.iterator().next().getMessage())
